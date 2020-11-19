48fd11bef21940d466666a6b95c379fb8e1cc14f
Brinkling96 <67553433+Brinkling96@users.noreply.github.com>
2020-11-19 17:46:45 -0500

    CheckTargetsForHeros

--- src/LegendsOfValor.java
+++ src/LegendsOfValor.java
@@ -180,6 +180,23 @@ public class LegendsOfValor extends MonsterGame {
     }
 
 
+    public ArrayList<Monster> checkTargets(Hero actor){
+        int row = actor.getRow();
+        int col = actor.getCol();
+        ArrayList<Monster> returnlist = new ArrayList<Monster>();
+        //Creature
+        for(int i  = row-1; i< row+1; i++){
+            for(int j = col-1; j < col+1; j++) {
+                for (Monster mon : Villains) {
+                    if (mon.getRow() == i && mon.getCol() == j) {
+                        returnlist.add(mon);
+                    }
+                }
+            }
+        return returnlist;
+    }
+
+
 
 
 
